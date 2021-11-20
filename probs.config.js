92edef74b2cf3d4bd9e6a7bb83fa183597d0a442
Jakob Rosenberg <jakobrosenberg@gmail.com>
2021-11-20 23:24:13 +0100

    fix: lots of fixes and refactors

---
+++ probs.config.js
@@ -0,0 +1,15 @@
+import { testDir } from "./test/util.js"
+import { dirname } from 'path'
+
+const replacePlaceholder = map => input => input.replace(/%(.+?)%/g, (_all, word) => map[word])
+
+
+export default {
+    setupFile: id => {
+        global.testDir = (name, args) => {
+            const map = { dirname: dirname(id) }
+            args = args.map(replacePlaceholder(map))
+            return testDir(`${dirname(id)}/_tests/${name}`, args)
+        }
+    }
+}
