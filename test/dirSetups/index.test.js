6cc8c412a59ab8a72d606560493de674607f561c
Jakob Rosenberg <jakobrosenberg@gmail.com>
2021-12-11 22:29:54 +0100

    feat: setupDir

---
+++ test/dirSetups/index.test.js
@@ -0,0 +1,10 @@
+import { rmSync, mkdirSync, writeFileSync, readdirSync, readFileSync } from 'fs'
+import { createDirname, delay } from "../util.js";
+const __dirname = createDirname(import.meta)
+const tmpDir = __dirname + '/temp'
+const file = tmpDir + '/timestamp.json'
+
+test('waits for dirSetup', () => {
+    const value = readFileSync(file, 'utf-8')
+    assert.equal(value, 'foo')
+})
