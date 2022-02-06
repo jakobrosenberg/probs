8802d9563ca6ed3392ca7523240b983bc52e11ea
Jakob Rosenberg <jakobrosenberg@gmail.com>
2022-02-06 17:12:34 +0100

    test: rename expect to expects

--- test/config-test/config.test.js
+++ test/config-test/config.test.js
@@ -2,11 +2,11 @@ test('individual globals for each file', () => {
     assert.equal(global.filename, 'test/config-test/config.test.js')
 })
 test('individual context for each test', (test, ctx) => {
-    const expect = 'test/config-test/config.test.js > individual context for each test'
-    assert.equal(ctx.scopeString, expect)
+    const expects = 'test/config-test/config.test.js > individual context for each test'
+    assert.equal(ctx.scopeString, expects)
 
     test('also in nested tests', (test, ctx) => {
-        const expect = 'test/config-test/config.test.js > individual context for each test > also in nested tests'
-        assert.deepEqual(ctx.scopeString, expect)
+        const expects = 'test/config-test/config.test.js > individual context for each test > also in nested tests'
+        assert.deepEqual(ctx.scopeString, expects)
     })
 })
