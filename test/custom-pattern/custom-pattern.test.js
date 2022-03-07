7e8af3f5b4571fbcf9a4fa09e58e677c4dba74cf
Jakob Rosenberg <jakobrosenberg@gmail.com>
2022-03-07 08:43:52 +0100

    chore: cleanup

--- test/custom-pattern/custom-pattern.test.js
+++
@@ -1,7 +0,0 @@
-import { createDirname, testDir } from "../util.js"
-const __dirname = createDirname(import.meta)
-
-test('before-all runs before all', ()=>{
-    const result = testDir(`${__dirname}/example`)
-    // console.log(result)
-})
